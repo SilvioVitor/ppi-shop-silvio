414f975cb406816616c69d04e1bff7c9ec4ae4fd
Silvio Victor do Nascimento Lima <silvio.victor@academico.ifrn.edu.br>
2024-12-05 07:48:16 -0300

    Search Box Done

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,4 +1,4 @@
-import { useContext, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import Product from "./Product";
 import { CircularProgress } from "@mui/material";
 import { CartContext } from "../context/CartContext";
@@ -11,15 +11,22 @@ export default function Shop() {
     const searchInput = useRef("");
     const [filteredItems, setFilteredItems] = useState([])
 
-    function handleSearch () {
+    useEffect(() => {
+        if (products) {
+            setFilteredItems(products)
+        }
+    }, [products]);
+
+    function handleSearch() {
         const term = searchInput.current.value.toLowerCase();
         setFilteredItems(
             products.filter((item) => item.title.toLowerCase().includes(term))
         )
     }
 
-    function clearSearch () {
+    function clearSearch() {
         searchInput.current.value = "";
+        setFilteredItems(products);
     }
 
     return (
@@ -28,12 +35,12 @@ export default function Shop() {
 
             <div className={styles.search_container}>
                 <div className={styles.search_box}>
-                    <input 
-                    ref={searchInput}
-                    className={styles.search_input}
-                    type="text"
-                    placeholder="Type to Search"
-                    onChange={handleSearch}
+                    <input
+                        ref={searchInput}
+                        className={styles.search_input}
+                        type="text"
+                        placeholder="Type to Search"
+                        onChange={handleSearch}
                     />
                     <button className={styles.search_clear} onClick={clearSearch}>
                         CLEAR
@@ -43,17 +50,20 @@ export default function Shop() {
 
             <ul id="products">
                 {error && <p>{error}</p>}
-                {!loading && products ? (
-                    products.map((product) => (
+                {loading &&
+                    <div id="loading">
+                        <CircularProgress size="10rem" color="inherit" />
+                        <p>Loading products...</p>
+                    </div>
+                }
+                {!loading && !error && filteredItems.length > 0 ? (
+                    filteredItems.map((product) => (
                         <li key={product.id}>
                             <Product {...product} />
                         </li>
                     ))
                 ) : (
-                    <div id="loading">
-                        <CircularProgress size="10rem" color="inherit" />
-                        <p>Loading products...</p>
-                    </div>
+                    <p>Not Found!</p>
                 )}
             </ul>
 
